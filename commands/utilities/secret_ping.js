24c7e1fc18789f47642f88a5566520e5a2a1b7a7
SHyguymoll <shymollet@gmail.com>
2024-01-27 18:05:35 -0800

    fixed secret_ping name

--- commands/utilities/secret_ping.js
+++ commands/utilities/secret_ping.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('secret ping')
+		.setName('secret_ping')
 		.setDescription('Replies with Pong, but only to you!'),
 	async execute(interaction) {
 		await interaction.reply({ content:'Pong!', ephemeral:true });

a60d1c867be4ea9073ccee97ff1edaa6bbaf447d
Brandon Tseng <noblesixforever@gmail.com>
2024-01-28 00:49:45 -0800

    Checks if resume is in an accepted file format, prints an error if it isnt

--- commands/main/grade.js
+++ commands/main/grade.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('Grade your resume with an ATS.')
 		.addAttachmentOption(option =>
 			option.setName('resume')
-				.setDescription('Attach the resume you want to grade (.pdf or .docx).')
+				.setDescription('Attach the resume you want to grade (.pdf .doc .docx).')
 				.setRequired(true))
 		.addBooleanOption(option =>
 			option.setName('private')
@@ -14,8 +14,20 @@ module.exports = {
 				.setRequired(true)),
 	async execute(interaction) {
 		const attachment = interaction.options.getAttachment('resume');
-		if (interaction.options.getBoolean('private') ?? true) {
-			await interaction.reply(`Received ${attachment.name}, creating thread...`);
+		// Checks for accepted file format.
+		if ((attachment.contentType == "application/pdf") || // .pdf 
+			(attachment.contentType == "application/msword") || // .doc
+			(attachment.contentType == "application/vnd.openxmlformats-officedocument.wordprocessingml.document")) { // .docx
+				if (interaction.options.getBoolean('private') ?? true) {
+					await interaction.reply(`Received ${attachment.name}, creating thread...`);
+				}
+		} else {
+			// Cancels resume grading if file is not in an accepted file format.
+			interaction.reply('Invalid file format! (Accepted formats: .pdf .doc .docx)')
+				.then(msg => {
+					// Deletes message after 5 seconds.
+					setTimeout(() => msg.delete(), 5000)
+				})
 		}
 	},
 };

ec5f81f91a56c4054b1c402c39147fc9fa60717c
SHyguymoll <shymollet@gmail.com>
2024-01-27 17:36:42 -0800

    adding commands via folder searching

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error(`${interaction.commandName} not found.`);
+		return;
+	}
+	try {
+		await command.execute(interaction);
+	}
+	catch (err) {
+		console.error(err);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'command failed to execute', ephemeral:true });
+		}
+		else {
+			await interaction.reply({ content: 'command failed to execute', ephemeral:true });
+		}
+	}
+});
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
